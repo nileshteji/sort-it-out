5d239fb2436e41666ca9c15f84be5222fdd9668b
Nilesh Teji <kumar.nilesh023@gmail.com>
2019-10-13 17:15:56 +0530

    Added

--- script/hello.sh
+++ script/hello.sh
@@ -1,13 +1,29 @@
+
+
+
+
+
 #!/bin/bash
-#this script sorts files according to their extensions
+
 oldIFS=$IFS
+
+
 IFS=$'\n'
+
 (find . -type f) > /tmp/temp
+
 for var in `cat /tmp/temp`
+
 do
+
 name=`basename "$var"`
+
 ext=`echo $name | cut -d'.' -f2- | cut -d'.' -f2- | cut -d'.' -f2- | cut -d'.' -f2- | cut -d'.' -f2- | cut -d'.' -f2- | cut -d'.' -f2-`
+
 mkdir -p $ext
+
 mv "$var" $ext/ 2> /dev/null
+
 done
+
 IFS=$oldIFS
